91b3ad46a1b8da4d7e01d1d42afa2c8522cd905e
Clayton <45052532+ClaytonP22DOTjpeg@users.noreply.github.com>
2025-03-07 22:46:18 -0500

    Version 0.01

---
+++ src/main/java/net/remag/remagsveinminer/RemagsVeinMinerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.remag.remagsveinminer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RemagsVeinMinerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
